74897ebda1191655f26cbe68e1b94aa2074014a6
alex <alex.alexalum@hotmail.com>
2020-06-19 01:54:36 -0500

    view importar clientes
    creacion de la vista para importar clientes

---
+++ resources/views/subir.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-8 mx-auto">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex justify-content-between">
+                    <div>
+                        Importar Clientes
+                    </div>
+                    <div>
+                        
+                        
+                    </div>
+
+                </div>
+                
+            </div>
+            <div class="card-body">
+
+<form action="upload" method="POST" enctype="multipart/form-data">
+	<input type="file" name="lista"/>
+	@csrf
+	<br>
+	<br>
+	<button type="submit"> subir archivo</button>
+</form>
+
+
+
+
+
+
+
+                
+            </div>
+            
+        </div>
+        
+    </div>
+</div>
+@endsection
